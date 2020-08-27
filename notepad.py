52e623beda0cec613900994fcda875652fb40517
aswnss-m <aswnss.m@gmail.com>
2020-08-27 22:08:02 +0530

    started modifiying notepad.py , using classes

--- notepad.py
+++ notepad.py
@@ -3,6 +3,95 @@ import pyperclip
 # initiallisation of the tkinter widget
 
 
+class Notepad:
+   def __init__(self,width = 800,height=800):
+      self.width=width
+      self.height =height
+      self.root = Tk()
+      self.root.geometry("{}x{}".format(self.width,self.height))
+      self.root.columnconfigure(1,weight=3)
+
+      entry1 = Text(self.root, width=self.width , height=self.height)
+      entry1.pack()
+
+      menubar =Menu(self.root)
+
+      filemenu = Menu(menubar,tearoff=0)
+      filemenu.add_command(label='Save',command=save_window)
+      menubar.add_cascade(label="File",menu=filemenu)
+      self.root.config(menu=menubar)
+      self.root.mainloop()
+
+   def savewindow(self):
+      window = Tk()
+      warn_window.geometry('300x100')
+      warn_window.title("Warning!!")
+      warn1 = Label(warn_window,text="File Already Exist!" ,font=("Ubuntu bold",10))
+      warnbut1=Button(warn_window,text="overwrite",command = self.write_over)
+      warnbut2=Button(warn_window,text="Append",command = self.append_over)
+      warnbut3=Button(warn_window,text="Close",command=warn_window.destroy())
+
+      warn1.pack()
+      warnbut1.pack()
+      warnbut2.pack()
+      warnbut3.pack()
+      warn_window.mainloop()
+      window.columnconfigure(0,weight=1)
+      window.columnconfigure(1,weight=2)
+
+      label1 = Label(window,test="FileName : ",padx=5,pady=5,sticky=E+W)
+      file = Entry(window,sticky=E+W)
+      self.filename=file.get() + '.txt'
+      self.content = entry1.get(1.0,"end-1c")
+
+      try:
+         f = open(self.filename,"x")
+         f = open(self.filename,"w") 
+      except FileExistsError:
+         self.warn()
+   def warn(self):
+	warn_window =Tk()
+	warn_window.configure(padx=10,pady=10)
+	warn_window.title("Warning!!")
+	warn1 = Label(warn_window,text="File Already Exist!" ,font=("Ubuntu bold",15))
+	warnbut1=Button(warn_window,text="overwrite",command = self.write_over)
+	warnbut2=Button(warn_window,text="Append",command = self.append_over)
+	warnbut3=Button(warn_window,text="Close",command=warn_window.destroy)
+
+	warn1.grid(row=0,column =0,columnspan=3 , ipady=5)
+	warnbut1.grid(row=1,column=0)
+	warnbut2.grid(row=1,column=1)
+	warnbut3.grid(row=1,column=2)
+	warn_window.mainloop()
+	def write_over(self):
+		f = open(self.filename , "w")
+		f.write(self.content)
+		f.close()
+	def append_over(self):
+		f = open(self.filename , "a")
+		f.write(self.content)
+		f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 root = Tk()
 root.title("Notepad --")
 root.geometry("800x800")
